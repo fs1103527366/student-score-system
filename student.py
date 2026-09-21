768e04970679bf95580c5af3942a2eb0687fafe2
oscar <oscar159@qq.com>
2026-09-21 09:52:59 +0800

    实现成绩统计功能，计算最高分最低分平均分

--- student.py
+++ student.py
@@ -1,6 +1,11 @@
-def show_student():
-    print("Student Management System")
-
+def score_stat(scores):
+    max_score = max(scores)
+    min_score = min(scores)
+    avg_score = sum(scores)/len(scores)
+    print(f"最高分：{max_score}")
+    print(f"最低分：{min_score}")
+    print(f"平均分：{avg_score:.2f}")
 
 if __name__ == "__main__":
-    show_student()
+    score_list = [85,92,78,66,90]
+    score_stat(score_list)
