75f2b019268c5a728780fac20331acb059338c74
sth <1562467018@qq.com>
2026-09-21 16:31:03 +0800

    组员B：新增学生班级输入输出功能

--- student.py
+++ student.py
@@ -3,6 +3,9 @@ def show_student():
     print(f"学生姓名：{name}")
     score = float(input("请输入学生成绩："))
     print(f"学生成绩：{score}")
+    # 组员B新增代码
+    clazz = input("请输入学生班级：")
+    print(f"学生班级：{clazz}")
 
 if __name__ == "__main__":
     show_student()
