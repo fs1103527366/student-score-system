77b459c11385efba49bd0e4d26a12266e307e91f
gzxmmz <1399332055@qq.com>
2026-09-21 13:30:07 +0800

    Add student gender input and output

--- student.py
+++ student.py
@@ -1,6 +1,9 @@
 def show_student():
-    print("Student Management System")
+    name = input("请输入学生姓名：")
+    gender = input("请输入学生性别：")
 
+    print(f"学生姓名：{name}")
+    print(f"学生性别：{gender}")
 
 if __name__ == "__main__":
     show_student()
